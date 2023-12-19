1976be32e98dfa6af2d7e409a1fcdf1052eda9f3
Leonardo <dervil.dev@gmail.com>
2023-12-19 16:46:56 +0000

    add todo components and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Learn Typescript and React',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Take off ticket for GCP',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'View TerraForm course',
+    completed: false
+  }
+]
 function App (): JSX.Element {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = (id: string): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
   return (
-    <h1>Todo MVC</h1>
+    <div className='todoapp'>
+      <Todos
+        onRemoveTodo={handleRemove}
+        todos={todos}
+       />
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import 'todomvc-app-css/index.css'
 import './index.css'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
